9c01312bd96716f4b29f0c4a00ef8d8c39ff71c7
Aidan Bailey <dev@aidanbailey.me>
2025-09-01 16:54:11 +0200

    Add 3VL Grammar to GRAMMARS

--- src/rapt2/treebrd/grammars/__init__.py
+++ src/rapt2/treebrd/grammars/__init__.py
@@ -1,4 +1,5 @@
 from .core_grammar import CoreGrammar
 from .extended_grammar import ExtendedGrammar
+from .threevl_grammar import ThreeVLGrammar
 
-GRAMMARS = {"Core Grammar": CoreGrammar, "Extended Grammar": ExtendedGrammar}
+GRAMMARS = {"Core Grammar": CoreGrammar, "Extended Grammar": ExtendedGrammar, "3VL Grammar": ThreeVLGrammar}
