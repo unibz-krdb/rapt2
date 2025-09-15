70cc9dd1fe62cbcbc31aae149afd0f8379eccd84
Aidan Bailey <dev@aidanbailey.me>
2025-09-15 10:38:25 +0200

    Refactor

--- src/rapt2/treebrd/grammars/syntax.py
+++ src/rapt2/treebrd/grammars/syntax.py
@@ -1,55 +1,56 @@
+from dataclasses import dataclass
+
+@dataclass(frozen=True)
 class Syntax:
+    # General tokens.
+    terminator = ";"
+    delim = ","
+    params_start = "_{"
+    params_stop = "}"
+    paren_left = "("
+    paren_right = ")"
+
+    # Logical tokens.
+    not_op = "not"
+    and_op = "and"
+    or_op = "or"
+
+    # Comparison operators.
+    equal_op = "="
+    not_equal_op = "!="
+    not_equal_alt_op = "<>"
+    less_than_op = "<"
+    less_than_equal_op = "<="
+    greater_than_op = ">"
+    greater_than_equal_op = ">="
+
+    # Relational algebra operators.
+    project_op = "\\project"
+    rename_op = "\\rename"
+    select_op = "\\select"
+    assign_op = ":="
+    join_op = "\\join"
+    theta_join_op = "\\theta_join"
+    natural_join_op = "\\natural_join"
+    full_outer_join_op = "\\full_outer_join"
+    left_outer_join_op = "\\left_outer_join"
+    right_outer_join_op = "\\right_outer_join"
+    difference_op = "\\difference"
+    union_op = "\\union"
+    intersect_op = "\\intersect"
+
+    # Defined operator.
+    defined_op = "defined"
+
+    # Dependency operators.
+    pk_op = "pk"
+    mvd_op = "mvd"
+    fd_op = "fd"
+    inc_equiv_op = "inc="
+    inc_subset_op = "inc⊆"
+
     def __init__(self, **kwargs):
-        # First initialize a default syntax.
-
-        # General tokens.
-        self.terminator = ";"
-        self.delim = ","
-        self.params_start = "_{"
-        self.params_stop = "}"
-        self.paren_left = "("
-        self.paren_right = ")"
-
-        # Logical tokens.
-        self.not_op = "not"
-        self.and_op = "and"
-        self.or_op = "or"
-
-        # Comparison operators.
-        self.equal_op = "="
-        self.not_equal_op = "!="
-        self.not_equal_alt_op = "<>"
-        self.less_than_op = "<"
-        self.less_than_equal_op = "<="
-        self.greater_than_op = ">"
-        self.greater_than_equal_op = ">="
-
-        # Relational algebra operators.
-        self.project_op = "\\project"
-        self.rename_op = "\\rename"
-        self.select_op = "\\select"
-        self.assign_op = ":="
-        self.join_op = "\\join"
-        self.theta_join_op = "\\theta_join"
-        self.natural_join_op = "\\natural_join"
-        self.full_outer_join_op = "\\full_outer_join"
-        self.left_outer_join_op = "\\left_outer_join"
-        self.right_outer_join_op = "\\right_outer_join"
-        self.difference_op = "\\difference"
-        self.union_op = "\\union"
-        self.intersect_op = "\\intersect"
-
-        # Defined operator.
-        self.defined_op = "defined"
-
-        # Dependency operators.
-        self.pk_op = "pk"
-        self.mvd_op = "mvd"
-        self.fd_op = "fd"
-        self.inc_equiv_op = "inc="
-        self.inc_subset_op = "inc⊆"
-
-        # Now set any user defined syntax.
+        # Set any user defined syntax.
         for key in kwargs:
             if hasattr(self, key):
                 setattr(self, key, kwargs[key])
